0425465722b27aecf5646d32599dd018dfde590a
sarahschoonmaker <srschoonmaker@gmail.com>
2023-09-26 09:05:46 -0400

    edited styles

--- src/components/Portfolio/styles.ts
+++ src/components/Portfolio/styles.ts
@@ -30,7 +30,7 @@ export const Container = styled.section`
       color: #FFF;
       &:hover{
         transform: translateY(-5px);
-        background-color: var(--pink);
+        background-color: var(--blue);
       }
     }
 
