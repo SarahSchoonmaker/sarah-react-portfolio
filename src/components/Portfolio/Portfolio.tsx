9f615a9b1f3f71208477bae041b1044a92d4f64a
sarahschoonmaker <srschoonmaker@gmail.com>
2023-09-27 13:09:38 -0400

    edited proejcts

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -149,6 +149,36 @@ export function Portfolio(){
             </footer>
           </div>
         </ScrollAnimation>
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+            <div className="project-links">
+              <a href="https://github.com/SarahSchoonmaker/hadoop-pipeline" target="_blank" rel="noreferrer">
+                <img src={githubIcon} alt="GitHub" />
+              </a>
+              
+            </div>
+            </header>
+            <div className="body">
+              <h3>Hadoop Data Pipeline</h3>
+              <p>Build an end to end data pipeline for a retail store called ABC-Stores using Hadoop 
+                for storage and spark for data processing to produce reports to perform analytics in Power BI<a href="https://github.com/SarahSchoonmaker/hadoop-pipeline"> View Code</a>.
+              
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list"> 
+              <li>Hadoop</li>
+              <li>Spark</li>
+              <li>Airflow</li>
+              <li>MySQL</li>
+        
+                 
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
 
         </div>
 
