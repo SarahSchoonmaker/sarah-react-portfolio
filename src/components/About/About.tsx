df7088d3d7046a369f1bba0e57a8679ec9628ddf
sarahschoonmaker <srschoonmaker@gmail.com>
2023-10-02 10:28:46 -0400

    edited readme

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -18,7 +18,7 @@ export function About(){
           <h2>About Me</h2>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-          <p>Data Engineer working with portfolio
+          <p>Data Engineer with data analysis experience working with portfolio
             managers to identify trends and build profitable strategies. 
             Pursuing an MS in Computer Science with a Machine Learning specialization at the Stevens Insitute of Technology. 
             In my free time, I am always learning something new, exercising at the gym, watching ice hockey (Go Rangers), 
