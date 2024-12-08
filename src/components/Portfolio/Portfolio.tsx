c2f7108c039df2dbb4c7ae5fdf7f7922121f9eff
sarahschoonmaker <srschoonmaker@gmail.com>
2024-12-07 19:12:03 -0500

    edited readme

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -73,59 +73,6 @@ export function Portfolio() {
                 role="img"
                 viewBox="0 0 24 24"
                 fill="none"
-                stroke="#23ce6b "
-                strokeWidth="1"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <title>Folder</title>
-                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-              </svg>
-              <div className="project-links">
-                <a
-                  href="https://github.com/IbrahimHiarea/Pet-Accessories-eCommerce-app"
-                  target="_blank"
-                  rel="noreferrer"
-                  aria-label="View project on GitHub"
-                >
-                  <img src={githubIcon} alt="GitHub" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <h3>Pet E-Commerce Store</h3>
-              <p>
-                This is a pet shop application written using Typescript, React,
-                and Redux.
-                <a
-                  href="https://github.com/IbrahimHiarea/Pet-Accessories-eCommerce-app"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {" "}
-                  View Code
-                </a>
-                .
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>React, Redux, Typescript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        {/* Project 3 */}
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg
-                width="50"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                viewBox="0 0 24 24"
-                fill="none"
                 stroke="#23ce6b"
                 strokeWidth="1"
                 strokeLinecap="round"
