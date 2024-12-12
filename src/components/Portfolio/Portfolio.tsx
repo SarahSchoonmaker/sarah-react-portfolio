f306c15394f97a8e2e280059a02035e46fceacd2
sarahschoonmaker <srschoonmaker@gmail.com>
2024-12-12 14:35:59 -0500

    edited readme

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -115,6 +115,115 @@ export function Portfolio() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        {/* Project 3 */}
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/SarahSchoonmaker/Spring-Pet-Clinic"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="View project on GitHub"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Spring Boot Pet Database</h3>
+              <p>
+                This database allows users to create and read owners and
+                veterinarians.
+                <a
+                  href="https://github.com/SarahSchoonmaker/Spring-Pet-Clinic"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  View Code
+                </a>
+                .
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Java 17</li>
+                <li>Spring Boot</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        {/* Project 3 */}
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/SarahSchoonmaker/flask-stock-data"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="View project on GitHub"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Stock Data Visualization</h3>
+              <p>
+                This Flask application allows users to enter the name of a
+                company and visualize its performance using Plotly.
+                <a
+                  href="https://github.com/SarahSchoonmaker/flask-stock-data"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  View Code
+                </a>
+                .
+              </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Flask</li>
+                <li>Alpha Vantage API</li>
+                <li>Plotly</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
       </div>
     </Container>
   );
